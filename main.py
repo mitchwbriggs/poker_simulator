4671675de1804d7c30467622028c6675792acea5
mitchwbriggs <80184015+mitchwbriggs@users.noreply.github.com>
2021-03-06 19:15:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ import holdem_engine as hldm
 import equity_calculator as eqty_calc
 import threading
 
+# see my.kv file for the core GUI code
 
 class MyGrid(Widget):
+    """Creating objects to represent the user's cards, the cards on the board, and the number of opponents in the hand"""
     user_cards = ObjectProperty(None)
     board_cards = ObjectProperty(None)
     num_opponents = ObjectProperty(None)
@@ -29,31 +31,44 @@ class SimplePopup(Popup):
 class CalcButton(Button):
 
     def fire_popup(self, user_card1, user_card2, board_card1, board_card2, board_card3, board_card4, board_card5, num_opponents):
-        """Method that creates the main window"""
+        """Method that controls the simulation process and generates output window"""
+        
+        # assigning GUI inputs to python variables
         user_card_names = [user_card1.text, user_card2.text]
         board_cards_input = [board_card1, board_card2, board_card3, board_card4, board_card5]
         board_cards_names = [card.text for card in board_cards_input if card.text is not 'None']
         num_opponents = int(num_opponents.text)
-
+        
+           
+        # creating a Deck object (see holdem_engine.py) 
+        deck = hldm.Deck()
+        
+        # creating Player objects (see holdem_engine.py) for each opponent left in the hand
         opponents = []
         for _ in range(num_opponents):
             opponent = hldm.Player()
             opponents.append(opponent)
-
-        deck = hldm.Deck()
+        
+        #Creating a Player object to represent the user (see holdem_engine.py)
         user = hldm.Player()
-
+        
+        # Matching the user's shortened card names (from GUI inputs) to Card objects in Deck (see holdem_engine.py)
         for name in user_card_names:
             [user.hole_cards.append(card) for card in deck.cards if card.name_short == name]
-
+        
+        # Matching the board's shortened card names (from GUI inputs) to Card objects in Deck (see holdem_engine.py)
         board_cards = []
         for name in board_cards_names:
             [board_cards.append(card) for card in deck.cards if card.name_short == name]
-
+        
+        # creating a Scenerio object (see equity_calculator.py) with input variables 
         scenario = eqty_calc.Scenario(user, opponents, board_cards)
+        
+        # creating Simulation object (see equity_calculator.py) and running 15,000 sims
         sim = eqty_calc.Simulation(scenario)
         win_pct = sim.run()
-
+        
+        # creating popup window that displays the user's estimated hand equity
         self.pop = SimplePopup()
         self.pop.open()
         self.pop.ids.eqty_output.text = win_pct
@@ -61,6 +76,7 @@ class CalcButton(Button):
 
 class MyApp(App): # <- Main Class
     def build(self):
+        """Builds the main GUI window"""
         return MyGrid()
 
 
